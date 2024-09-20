95b7d43b58cec618cd116f4f9b12ed97c7e50530
Minkyu Lee <whizkyu@snu.ac.kr>
2024-09-21 02:32:28 +0900

    .md HMR 적용

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,41 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 
+function SlidesHmr() {
+  return {
+    name: "slides-hmr",
+    enforce: "post",
+    // HMR
+    handleHotUpdate({ file, server }) {
+      if (file.endsWith(".md")) {
+        console.log("reloading markdown...");
+
+        server.ws.send({
+          type: "full-reload",
+          path: "*",
+        });
+      }
+    },
+  };
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [SlidesHmr()],
   build: {
     rollupOptions: {
       input: {
-        "main": "index.html",
+        main: "index.html",
         ...Object.fromEntries(
-        globSync("decks/**/*.html").map((file) => [
-          path.relative(
-            "decks",
-            file.slice(0, file.length - path.extname(file).length)
-          ),
-          fileURLToPath(new URL(file, import.meta.url)),
-        ])
-      )},
+          globSync("decks/**/*.html").map((file) => [
+            path.relative(
+              "decks",
+              file.slice(0, file.length - path.extname(file).length)
+            ),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ])
+        ),
+      },
     },
   },
 });
