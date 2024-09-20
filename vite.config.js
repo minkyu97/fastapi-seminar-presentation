0fe9112d60dcade646a4d7df774a373b93fbe026
Minkyu Lee <whizkyu@snu.ac.kr>
2024-09-21 01:45:19 +0900

    md 파일 public 폴더로 이동

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  assetsInclude: ["**/*.md"],
   build: {
     rollupOptions: {
       input: Object.fromEntries(
