0332b729580b18b681fe3f5a1ab54840b5895cdd
Minkyu Lee <whizkyu@snu.ac.kr>
2024-09-22 01:01:26 +0900

    Speaker 플러그인 추가

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Reveal from "reveal.js";
 import RevealHighlight from "reveal.js/plugin/highlight/highlight.esm.js";
 import RevealMarkdown from "reveal.js/plugin/markdown/markdown.esm.js";
+import RevealNotes from "reveal.js/plugin/notes/notes.esm.js";
 
 Reveal.initialize({
-  plugins: [RevealMarkdown, RevealHighlight],
+  plugins: [RevealMarkdown, RevealHighlight, RevealNotes],
   slideNumber: true,
   showNotes: true,
   pdfSeparateFragments: false,
